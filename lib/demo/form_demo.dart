061f3af5209ff98c5f021873ca8fa5b701e84b6b
Yun.kou <koopking@gmial.com>
2019-06-22 13:05:44 +0800

    添加form表单

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -13,10 +13,101 @@ class FormDemo extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextFieldDemo(),
+              RegisterForm(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final registerFormKey = GlobalKey<FormState>();
+  String username, password;
+  bool autovalidate = false;
+
+  void submitRegisterForm() {
+    if (registerFormKey.currentState.validate()) {
+      registerFormKey.currentState.save();
+
+      debugPrint('username: $username');
+      debugPrint('password: $password');
+
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Registering...'),
+        ),
+      );
+    } else {
+      setState(() {
+        autovalidate = true;
+      });
+    }
+  }
+
+  String validatorUsername(value) {
+    if (value.isEmpty) {
+      return 'Username is required';
+    }
+
+    return null;
+  }
+
+  String validatorPassword(value) {
+    if (value.isEmpty) {
+      return 'Password is required';
+    }
+
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: registerFormKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Username',
+            ),
+            onSaved: (value) {
+              username = value;
+            },
+            validator: validatorUsername,
+            autovalidate: true,
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Password',
+            ),
+            onSaved: (value) {
+              password = value;
+            },
+            validator: validatorPassword,
+            autovalidate: true,
+          ),
+          SizedBox(height: 32.0),
+          Container(
+            width: double.infinity,
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              child: Text(
+                'Register',
+                style: TextStyle(color: Colors.white),
+              ),
+              elevation: 0.0,
+              onPressed: submitRegisterForm,
+            ),
+          ),
+        ],
       ),
     );
   }
